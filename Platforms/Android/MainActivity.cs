79a46905adf2e251224ae1318b4c08fa8779fe3e
Demi <162566586+deems3@users.noreply.github.com>
2024-11-26 12:23:23 +0100

    made statusbar invisible

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,11 +1,19 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace TruthOrDrinkDemiBruls
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Zorg dat de statusbalk wordt verborgen
+            Window?.AddFlags(WindowManagerFlags.Fullscreen);
+        }
     }
 }
