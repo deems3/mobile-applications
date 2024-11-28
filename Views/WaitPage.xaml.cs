0cf69d6028e1ffa436c7e1f5f7a2758fae24d6b5
Demi <162566586+deems3@users.noreply.github.com>
2024-11-28 20:29:51 +0100

    added next question and stop button

--- Views/WaitPage.xaml.cs
+++ Views/WaitPage.xaml.cs
@@ -6,4 +6,15 @@ public partial class WaitPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+
+    private async void GoToNextQuestion(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("GameQuestions");
+    }
+
+    private async void GoToMain(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("Home");
+    }
 }
