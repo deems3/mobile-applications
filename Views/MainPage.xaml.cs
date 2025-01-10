e50f07f5d8705a40cb377adc5fad61f0c3a47635
Demi Bruls <1919822bruls@zuyd.nl>
2025-01-10 08:29:29 +0100

    gyroscope test

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -8,6 +8,7 @@ namespace TruthOrDrinkDemiBruls.Views
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
 
         private async void CreateGame(object sender, EventArgs e)
@@ -29,6 +30,37 @@ namespace TruthOrDrinkDemiBruls.Views
         private async void OpenDatabaseBrowser(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new DatabaseBrowserPage(Helper.GetDbPath("truth_or_drink_demi.db")));
+        }
+
+        private void ToggleGyroscope()
+        {
+            if (Gyroscope.Default.IsSupported)
+            {
+                if (!Gyroscope.Default.IsMonitoring)
+                {
+                    // Turn on gyroscope
+                    Gyroscope.Default.ReadingChanged += Gyroscope_ReadingChanged;
+                    Gyroscope.Default.Start(SensorSpeed.UI);
+                }
+                else
+                {
+                    // Turn off gyroscope
+                    Gyroscope.Default.Stop();
+                    Gyroscope.Default.ReadingChanged -= Gyroscope_ReadingChanged;
+                }
+            }
+        }
+
+        private void Gyroscope_ReadingChanged(object sender, GyroscopeChangedEventArgs e)
+        {
+            // Update UI Label with gyroscope state
+            GyroscopeLabel.TextColor = Colors.Green;
+            GyroscopeLabel.Text = $"Gyroscope: {e.Reading}";
+        }
+
+        private void ToggleMagnetometer(object sender, EventArgs e)
+        {
+            ToggleGyroscope();
         }
     }
 }
