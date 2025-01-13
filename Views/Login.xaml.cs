a74d2e56c15217fa8489340c5ac2ebc5745bb0bc
Demi Bruls <1919822bruls@zuyd.nl>
2025-01-13 07:08:21 +0100

    implement secure storage with more secure key

--- Views/Login.xaml.cs
+++ Views/Login.xaml.cs
@@ -4,11 +4,20 @@ public partial class Login : ContentPage
 {
 	public Login()
 	{
-		InitializeComponent();
+        var hasLoggedInBefore = SecureStorage.GetAsync("truth_or_drink_authentication_code").GetAwaiter().GetResult();
+        if (hasLoggedInBefore != null)
+        {
+            Shell.Current.GoToAsync("//MainPage").GetAwaiter().GetResult();
+            return;
+        }
+
+        InitializeComponent();
 	}
 
 	private async void LoginUser(object sender, EventArgs e)
 	{
+		await SecureStorage.SetAsync("truth_or_drink_authentication_code", Guid.NewGuid().ToString());
+
 		// the // betekent: er is geen backbutton en de hardware backbutton die gaat ook niet terug naar de vorige pagina.
 		Vibration.Default.Vibrate(1000);
 		await Shell.Current.GoToAsync("//MainPage");
